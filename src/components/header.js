e9339f46c5e2494a527d20e6dc85204ebb4f41b7
Wong Heung Sang <hswongac@gmail.com>
2018-06-24 16:36:26 +0800

    Change header link to overview

--- src/components/header.js
+++ src/components/header.js
@@ -17,7 +17,7 @@ const Header = ({ siteTitle }) => (
     >
       <h1 style={{ margin: 0 }}>
         <Link
-          to="/"
+          to="/overview/"
           style={{
             color: 'white',
             textDecoration: 'none',
